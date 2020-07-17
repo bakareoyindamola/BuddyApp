c4cab016c11d3923b7bf9405372eed9270ffc561
Bakare Oyindamola <f.inaku@tropicdreams.com.ng>
2020-07-17 02:42:35 +0100

    Created pros section and add form validation

---
+++ static/js/timer.js
@@ -0,0 +1,31 @@
+function launch() {
+    let now = new Date();
+    let launchDate = new Date(2020, 07, 30);
+
+    let currentTime = now.getTime();
+    let launchTime = launchDate.getTime();
+
+    let remTime = launchTime - currentTime;
+
+    let s = Math.floor(remTime / 1000);
+    let m = Math.floor(s / 60);
+    let h = Math.floor(m / 60);
+    let d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").textContent = d;
+    document.getElementById("hours").textContent = h;
+    document.getElementById("minutes").textContent = m;
+    document.getElementById("seconds").textContent = s;
+
+    setTimeout(launch, 1000);
+}
+
+launch()
